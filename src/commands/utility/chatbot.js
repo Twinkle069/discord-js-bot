ea264cd159a77dbc1824fd1be6389ab8f2da5edd
Twinkle069 <159515125+Twinkle069@users.noreply.github.com>
2024-04-21 13:55:11 +0530

    Update chatbot.js, Added multiple language support

--- src/commands/utility/chatbot.js
+++ src/commands/utility/chatbot.js
@@ -1,6 +1,34 @@
 const { ApplicationCommandOptionType, ChannelType, EmbedBuilder } = require("discord.js");
 const { EMBED_COLORS } = require("@root/config");
 
+const languages = [
+  { name: "Korean", value: "ko" },
+  { name: "English", value: "en" },
+  { name: "Japanese", value: "ja" },
+  { name: "Spanish", value: "es" },
+  { name: "French", value: "fr" },
+  { name: "German", value: "de" },
+  { name: "Italian", value: "it" },
+  { name: "Portuguese", value: "pt" },
+  { name: "Russian", value: "ru" },
+  { name: "Chinese (Simplified)", value: "zh-cn" },
+  { name: "Chinese (Traditional)", value: "zh-tw" },
+  { name: "Hindi", value: "hi" },
+  { name: "Arabic", value: "ar" },
+  { name: "Dutch", value: "nl" },
+  { name: "Greek", value: "el" },
+  { name: "Polish", value: "pl" },
+  { name: "Swedish", value: "sv" },
+  { name: "Turkish", value: "tr" },
+  { name: "Vietnamese", value: "vi" },
+  { name: "Finnish", value: "fi" },
+  { name: "Danish", value: "da" },
+  { name: "Norwegian", value: "no" },
+  { name: "Indonesian", value: "id" },
+  { name: "Malay", value: "ms" },
+  { name: "Bengali", value: "bn" },
+];
+
 /**
  * @type {import("@structures/Command")}
  */
@@ -11,7 +39,7 @@ module.exports = {
   userPermissions: ["ManageGuild"],
   command: {
     enabled: true,
-    usage: "set [channel ID] or chatbot delete",
+    usage: "<set | delete> <language>",
     minArgsCount: 1,
   },
   slashCommand: {
@@ -30,6 +58,13 @@ module.exports = {
             type: ApplicationCommandOptionType.Channel,
             channelTypes: [ChannelType.GuildText],
           },
+          {
+            name: "language",
+            description: "Choose language",
+            required: false,
+            type: ApplicationCommandOptionType.String,
+            choices: languages.map(lang => ({ name: lang.name, value: lang.value })),
+          },
         ],
       },
       {
@@ -37,80 +72,122 @@ module.exports = {
         description: "Delete chatbot channel",
         type: ApplicationCommandOptionType.Subcommand,
       },
+      {
+        name: "language",
+        description: "Update chatbot language",
+        type: ApplicationCommandOptionType.Subcommand,
+        options: [
+          {
+            name: "select_lang",
+            description: "Choose language",
+            required: true,
+            type: ApplicationCommandOptionType.String,
+            choices: languages.map(lang => ({ name: lang.name, value: lang.value })),
+          },
+        ],
+      },
     ],
   },
 
-  async messageRun(message, args, data) {
-    const input = args[0].toLowerCase();
-    const response = await chatbot(message, input, data);
-    await message.safeReply(response);
+  async messageRun(message, args) {
+    const embed = new EmbedBuilder()
+      .setColor(EMBED_COLORS.ERROR)
+      .setDescription("This command is only available as a slash command </chatbot set:1231487088768192596>");
+
+    await message.safeReply({ embeds: [embed] });
   },
-    
+
   async interactionRun(interaction, data) {
-    const subcommand = interaction.options.getSubcommand();
-    const response = await chatbot(interaction, subcommand, data);
+    const response = await chatbot(data, interaction);
     await interaction.followUp(response);
-  },
+  }
 };
 
 /**
- * @param {import("discord.js").CommandInteraction|import("discord.js").Message} arg0
- * @param {string} input
+ * @param {import("discord.js").CommandInteraction|import("discord.js").Message} data
  */
-async function chatbot({ client, guildId, options, channel }, input, data) {
-  if (input !== "set" && input !== "delete") {
-    const embed = new EmbedBuilder()
-      .setColor(EMBED_COLORS.ERROR)
-      .setDescription("Invalid option. Please use `set` or `delete`.");
+async function chatbot(data, interaction) {
+  const subcommand = interaction.options.getSubcommand();
+  const settings = data.settings;
 
-    return { embeds: [embed] };
-  }
+  if (subcommand === "set") {
+    let Chatchannel = interaction.options.getChannel("channel") || interaction.channel;
+    let language = interaction.options.getString("language") ?? "en";
 
-  const sub = input;
+    if (!settings.chatbotId) {
+      settings.chatbotId = Chatchannel.id;
+      settings.chatbotLang = language;
+      await settings.save();
 
-  try {
-    if (sub === "set") {
-      let targetChannel = options?.getChannel("channel") || channel;
-
-      if (!data.settings.chatbotId) {
-        data.settings.chatbotId = targetChannel.id;
-        await data.settings.save();
-
-        const embed = new EmbedBuilder()
-          .setColor(EMBED_COLORS.SUCCESS)
-          .setDescription(`Chatbot channel set to ${targetChannel}.`);
-        
-        return { embeds: [embed] };
-      } else {
-        const embed = new EmbedBuilder()
-          .setColor(EMBED_COLORS.ERROR)
-          .setDescription(`Chatbot is already set in this guild. Current channel: <#${data.settings.chatbotId}>`);
-        
-        return { embeds: [embed] };
-      }
-    } else if (sub === "delete") {
-      if (!data.settings.chatbotId) {
-        const embed = new EmbedBuilder()
-          .setColor(EMBED_COLORS.ERROR)
-          .setDescription("No chatbot setup found in this guild.");
-        
-        return { embeds: [embed] };
-      }
-
-      data.settings.chatbotId = undefined;
-      await data.settings.save();
-
+      const langName = getLangName(language);
       const embed = new EmbedBuilder()
         .setColor(EMBED_COLORS.SUCCESS)
-        .setDescription("Chatbot channel deleted successfully.");
-
+        .setDescription(`Chatbot channel set to ${Chatchannel} with language ${langName}.`);
+      
+      return { embeds: [embed] };
+    } else {
+      const embed = new EmbedBuilder()
+        .setColor(EMBED_COLORS.ERROR)
+        .setDescription(`Chatbot is already set in this guild. Current channel: <#${settings.chatbotId}>`);
+      
       return { embeds: [embed] };
     }
-  } catch (error) {
-    const embed = new EmbedBuilder()
-      .setColor(EMBED_COLORS.ERROR)
-      .setDescription(`Failed to perform the operation. Error: ${error.message}`);
+  } else if (subcommand === "delete") {
+    if (!data.settings.chatbotId) {
+      const embed = new EmbedBuilder()
+        .setColor(EMBED_COLORS.ERROR)
+        .setDescription("No chatbot setup found in this guild.");
+      
+      return { embeds: [embed] };
+    }
 
+    settings.chatbotId = undefined;
+    settings.chatbotLang = undefined;
+    await settings.save();
+
+    const embed = new EmbedBuilder()
+      .setColor(EMBED_COLORS.SUCCESS)
+      .setDescription("Chatbot channel deleted successfully.");
+
+    return { embeds: [embed] };
+  } else if (subcommand === "language") {
+    let language = interaction.options.getString("select_lang") ?? "en";
+
+    if (!settings.chatbotId) {
+      const embed = new EmbedBuilder()
+        .setColor(EMBED_COLORS.ERROR)
+        .setDescription("No chatbot setup found in this guild.");
+      
+      return { embeds: [embed] };
+    }
+
+    if (language === settings.chatbotLang) {
+      const embed = new EmbedBuilder()
+        .setColor(EMBED_COLORS.ERROR)
+        .setDescription(`Chatbot language is already set to ${getLangName(language)}.`);
+      
+      return { embeds: [embed] };
+    }
+
+    settings.chatbotLang = language;
+    await settings.save();
+
+    const langName = getLangName(language);
+    const embed = new EmbedBuilder()
+      .setColor(EMBED_COLORS.SUCCESS)
+      .setDescription(`Chatbot language updated to ${langName}.`);
+    
     return { embeds: [embed] };
   }
 }
+
+/**
+ * Function to get the language name from the language code
+ * @param {string} code - Language code
+ * @returns {string} - Language name
+ */
+function getLangName(code) {
+  const language = languages.find(lang => lang.value === code);
+  return language ? language.name : "English";
+   }
+  
